321db0a054b5088b6f9364653f7e66ccf6498fcf
lukin921 <lukin921@student.liu.se>
2024-12-01 15:07:35 +0900

    day 1 p2 remove sort

--- 2024/01/1-2.cpp
+++ 2024/01/1-2.cpp
@@ -11,7 +11,6 @@ int main() {
         v1.push_back(a);
         ++v2[b];
     }
-    sort(v1.begin(), v1.end());
     int ans = 0;
     for (int i = 0; i < v1.size(); ++i) {
         ans += v1[i] * v2[v1[i]];
