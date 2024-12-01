f176f9a10a2d030480723ee4f2f60beccb786ede
lukin921 <lukin921@student.liu.se>
2024-12-01 15:05:28 +0900

    day 1 done

---
+++ 2024/01/1-2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(false);
+
+    vector<int> v1; 
+    map<int, int> v2;
+    int a, b;
+    while (cin >> a >> b) {
+        v1.push_back(a);
+        ++v2[b];
+    }
+    sort(v1.begin(), v1.end());
+    int ans = 0;
+    for (int i = 0; i < v1.size(); ++i) {
+        ans += v1[i] * v2[v1[i]];
+    }
+    cout << ans;
+
+    return 0;
+}
