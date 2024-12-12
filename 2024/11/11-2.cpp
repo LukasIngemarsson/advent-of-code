6eb85596bcdef367c114e23a68fc07b696f5d5ef
lukin921 <lukin921@student.liu.se>
2024-12-12 10:46:46 +0900

    inverse dfs iter logic for intuition

--- 2024/11/11-2.cpp
+++ 2024/11/11-2.cpp
@@ -12,22 +12,22 @@ int main() {
 
     map<pair<string, int>, long long> state;
     auto dfs = [&] (auto&& dfs, string s, int iter) -> long long {
-        if (iter == 75) {
+        if (iter == 0) {
             return 1;
         }
         if (state.find({s, iter}) == state.end()) {
             long long res{};
             if (s == "0") {
-                res += dfs(dfs, "1", iter + 1);
+                res += dfs(dfs, "1", iter - 1);
             }
             else if (s.size() % 2 == 0) {
                 string left = s.substr(s.size() / 2, s.size());
                 string right = s.substr(0, s.size() / 2);
-                res += dfs(dfs, to_string(stoll(left)), iter + 1);
-                res += dfs(dfs, to_string(stoll(right)), iter + 1);
+                res += dfs(dfs, to_string(stoll(left)), iter - 1);
+                res += dfs(dfs, to_string(stoll(right)), iter - 1);
             }
             else {
-                res += dfs(dfs, to_string(stoll(s) * 2024), iter + 1);
+                res += dfs(dfs, to_string(stoll(s) * 2024), iter - 1);
             }
             state[{s, iter}] = res;
         }
@@ -36,7 +36,7 @@ int main() {
 
     long long ans{};
     for (string s : stones) {
-        ans += dfs(dfs, s, 0);
+        ans += dfs(dfs, s, 75);
     }
     cout << ans << endl;
 
