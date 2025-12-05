f61a842627aac51c5a60ca968f3d3cefcefd567c
lukin921 <lukin921@student.liu.se>
2025-12-05 14:05:43 +0100

    2025 day 5 done

---
+++ 2025/05.py
@@ -0,0 +1,61 @@
+import sys
+
+
+def p1(lines):
+    ranges = []
+    q_idx = -1
+
+    for i, line in enumerate(lines):
+        if line == '':
+            q_idx = i + 1
+            break
+
+        rng = tuple(map(int, line.split('-')))
+        ranges.append(rng)
+
+    assert q_idx != -1
+
+    fresh = set()
+
+    for line in lines[q_idx:]:
+        q = int(line)
+
+        for a, b in ranges:
+            if a <= q <= b:
+                fresh.add(q)
+
+    ans = len(fresh)
+    print(ans)
+
+
+def p2(lines):
+    ranges = []
+
+    for line in lines:
+        if line == '':
+            break
+
+        rng = tuple(map(int, line.split('-')))
+        ranges.append(rng)
+
+    ranges.sort()
+
+    end = -1
+    ans = 0
+
+    for a, b in ranges:
+        if b <= end:
+            continue
+
+        ans += b - max(a, end) + (end < a)
+        end = b
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    lines = sys.stdin.read().splitlines()
+
+    p1(lines)
+    p2(lines)
+
